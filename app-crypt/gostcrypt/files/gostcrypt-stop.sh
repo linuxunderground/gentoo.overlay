6074960e8db3c345fe974968217dd7fac77e3164
Vincent Hardy <vincent.hardy.be@gmail.com>
2017-10-12 20:37:51 +0200

    Not maintained anymore

--- app-crypt/gostcrypt/files/gostcrypt-stop.sh
+++
@@ -1,9 +0,0 @@
-# Copyright 1999-2015 Gentoo Foundation
-# Distributed under the terms of the GNU General Public License v2 or later
-
-# Try to remove any dm-crypt mappings
-if [ -x /usr/bin/gostcrypt ]; then
-	ebegin "Removing GostCrypt mappings"
-	! /usr/bin/gostcrypt -l > /dev/null 2>&1  || /usr/bin/gostcrypt -d
-	eend $?
-fi
