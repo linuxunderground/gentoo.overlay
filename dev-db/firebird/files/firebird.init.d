30be8c5ce46d062cb36196b418d066fa8ee67dd3
Vincent Hardy <vincent.hardy.be@gmail.com>
2018-05-10 09:28:33 +0200

    if the first connection try is non-root embedded : tmp dir is created with bad owner and group

--- dev-db/firebird/files/firebird.init.d
+++ dev-db/firebird/files/firebird.init.d
@@ -1,12 +1,12 @@
 #!/sbin/openrc-run
-# Copyright 1999-2017 Gentoo Foundation
+# Copyright 1999-2018 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License, v2 or later
-# $Id$
 
 name=${RC_SVCNAME}
 
 fbhomedir="/var/run/firebird"
 fblogdir="/var/log/firebird"
+fbtmpdir="/tmp/firebird"
 pidfile="${fbhomedir}/firebird.pid"
 command="/usr/sbin/fbguard"
 command_args="-forever -daemon -pidfile $pidfile"
@@ -20,4 +20,5 @@ depend() {
 start_pre() {
 	checkpath -q -d -m 0770 -o ${FBUSER:-firebird}:${FBGROUP:-firebird} ${fbhomedir} || return 1
 	checkpath -q -d -m 0770 -o ${FBUSER:-firebird}:${FBGROUP:-firebird} ${fblogdir} || return 1
+	checkpath -q -d -m 0770 -o ${FBUSER:-firebird}:${FBGROUP:-firebird} ${fbtmpdir} || return 1
 }
